cc6f48999a8e4518b799d744620ff3b4ec3ace43
Aditya Chandra <inibukanadit@gmail.com>
2019-10-28 20:48:15 +0700

    add markerModel and homedashboard viewmodel

---
+++ app/src/main/java/com/codedirect/trafficnetsecurity/ui/homedashboard/HomeDashboardViewModel.kt
@@ -0,0 +1,41 @@
+package com.codedirect.trafficnetsecurity.ui.homedashboard
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.viewModelScope
+import com.codedirect.trafficnetsecurity.data.remote.response.SensorData
+import com.codedirect.trafficnetsecurity.data.repo.SensorRepository
+import com.codedirect.trafficnetsecurity.data.util.orZero
+import com.codedirect.trafficnetsecurity.model.MarkerModel
+import com.codedirect.trafficnetsecurity.ui.AppViewModel
+import kotlinx.coroutines.launch
+
+class HomeDashboardViewModel(val sensorRepository: SensorRepository) : AppViewModel() {
+
+    private val mSensorList by lazy { MutableLiveData<List<SensorData>>() }
+
+    val sensorList by lazy {
+        Transformations.map(
+            mSensorList
+        ) { data ->
+            data.map {
+                MarkerModel(
+                    it.id.orEmpty(),
+                    it.lat.orZero().toDouble(),
+                    it.jsonMemberLong.orZero().toDouble()
+                )
+            }
+        }
+    }
+
+    override fun init() {
+        fetchSensorList()
+    }
+
+    private fun fetchSensorList() {
+        viewModelScope.launch {
+            mSensorList.value = sensorRepository.getSensorList()
+        }
+    }
+
+}
