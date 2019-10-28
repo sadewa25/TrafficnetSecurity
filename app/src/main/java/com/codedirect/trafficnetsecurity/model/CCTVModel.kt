31f3490c561c1b0bc805c46f39955b3e92281a79
Aditya Chandra <inibukanadit@gmail.com>
2019-10-28 20:48:50 +0700

    remove unused models

--- app/src/main/java/com/codedirect/trafficnetsecurity/model/CCTVModel.kt
+++
@@ -1,9 +0,0 @@
-package com.codedirect.trafficnetsecurity.model
-
-data class CCTVModel(
-    val id : Int,
-    val ssid : String,
-    val pass : String,
-    val info: String,
-    val status: String
-)
