dfcfb626eeeb0d8316bee37c713542d92fa9e3af
Aditya Chandra <inibukanadit@gmail.com>
2019-10-31 09:10:20 +0700

    refactor HomeSensorFragment codes

--- app/src/main/java/com/codedirect/trafficnetsecurity/ui/homesensor/HomeSensorFragment.kt
+++ app/src/main/java/com/codedirect/trafficnetsecurity/ui/homesensor/HomeSensorFragment.kt
@@ -9,10 +9,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.viewpager.widget.ViewPager
-import com.codedirect.trafficnetsecurity.ui.createsensor.CreateSensorActivity
-import com.codedirect.trafficnetsecurity.ui.cctvlist.CCTVListFragment
-import com.codedirect.trafficnetsecurity.ui.sensorlist.SensorListFragment
 import com.codedirect.trafficnetsecurity.R
+import com.codedirect.trafficnetsecurity.ui.cctvlist.CCTVListFragment
+import com.codedirect.trafficnetsecurity.ui.createsensor.CreateSensorActivity
+import com.codedirect.trafficnetsecurity.ui.sensorlist.SensorListFragment
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_home_sensor.*
 
 /**
@@ -20,36 +21,36 @@ import kotlinx.android.synthetic.main.fragment_home_sensor.*
  */
 class HomeSensorFragment : Fragment() {
 
+    private lateinit var starterIntent: Intent
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home_sensor, container, false)
-    }
+    ): View? = inflater.inflate(R.layout.fragment_home_sensor, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var starterIntent: Intent = Intent(activity, CreateSensorActivity::class.java)
+        btn_add.setOnClickListener { startActivity(starterIntent) }
 
-        btn_add.setOnClickListener {
-            startActivity(starterIntent)
+        setupViewPager(vp_sensor, tabs_sensor, fragmentManager)
+    }
 
+    private fun setupViewPager(
+        viewPager: ViewPager,
+        tabLayout: TabLayout,
+        manager: FragmentManager?
+    ) {
+
+        viewPager.adapter = HomeSensorViewPagerAdapter(manager).apply {
+            addFragment(SensorListFragment(), context?.getString(R.string.title_sensors)!!)
+            addFragment(CCTVListFragment(), context?.getString(R.string.title_cctv)!!)
         }
 
-        setupViewPager(vp_sensor, fragmentManager)
-        tabs_sensor.setupWithViewPager(vp_sensor)
-        vp_sensor.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-            override fun onPageScrollStateChanged(state: Int) {
-            }
+        tabLayout.setupWithViewPager(viewPager)
 
-            override fun onPageScrolled(
-                position: Int,
-                positionOffset: Float,
-                positionOffsetPixels: Int
-            ) {
-            }
-
+        viewPager.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener() {
             override fun onPageSelected(position: Int) {
                 if (position == 0) {
                     tv_main_sensor_title.text = getString(R.string.main_title_sensor)
@@ -60,22 +61,10 @@ class HomeSensorFragment : Fragment() {
                     tv_main_sensor_title.text = getString(R.string.main_title_cctv)
                     tv_main_sensor_subtitle.text = getString(R.string.subtitle_cctv)
                     btn_add.text = getString(R.string.btn_cctv)
-//                   starterIntent = Intent(activity, RegisterCctvActivity::class.java)
+                    starterIntent = Intent(activity, CreateSensorActivity::class.java)
                 }
             }
-
         })
-
     }
-
-    fun setupViewPager(viewPager: ViewPager, manager: FragmentManager?) {
-        var adapter: HomeSensorViewPagerAdapter =
-            HomeSensorViewPagerAdapter(manager)
-        adapter.addFragment(SensorListFragment(), context?.getString(R.string.title_sensors)!!)
-        adapter.addFragment(CCTVListFragment(), context?.getString(R.string.title_cctv)!!)
-
-        viewPager.adapter = adapter
-    }
-
 
 }
